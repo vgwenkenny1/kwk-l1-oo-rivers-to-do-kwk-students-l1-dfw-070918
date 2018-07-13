fe56272faf605e7f9fd049afa3ea2f736c629020
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-13 18:40:13 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,22 @@
-# river.rb
+class River
+
+attr_reader :name, :length, :countries, :discharge
+attr_writer :name, :length, :countries, :discharge
+  
+def initialize(name)
+  @name = name 
+  @length = length 
+  @countries = countries 
+end 
+
+def flood
+  @discharge *= 1.3
+end 
+
+def dry_up
+  @discharge *= 0.5
+end 
+
+
+
+end 
